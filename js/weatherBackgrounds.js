742d7c5c9829de7aa5d4b7f0097f8c4bd1fa2370
Chida <singh.chida2004@gmail.com>
2025-03-27 15:55:32 +0530

    Added js code

---
+++ js/weatherBackgrounds.js
@@ -0,0 +1,11 @@
+const weatherBackgrounds = {
+    sunny: 'from-yellow-400 to-orange-500',
+    clear: 'from-blue-400 to-blue-600',
+    cloudy: 'from-gray-400 to-gray-600',
+    rain: 'from-indigo-600 to-blue-500',
+    drizzle: 'from-blue-500 to-blue-700',
+    thunderstorm: 'from-gray-800 to-black',
+    snow: 'from-gray-100 to-gray-300',
+    mist: 'from-gray-500 to-gray-700',
+    default: 'from-purple-500 to-indigo-600'
+};
